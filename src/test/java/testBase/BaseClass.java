fc6bb26fe98652ab451d30988af4908b08e817e4
shewavik <vikram.shewale@tietoevry.com>
2025-09-13 17:47:15 +0530

    Selenium grid changes

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,13 +3,13 @@ package testBase;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -18,6 +18,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -38,11 +39,39 @@ public class BaseClass {
         logger = LogManager.getLogger(this.getClass());
         logger.info("Initializing Driver...");
 
-        switch (browser.toLowerCase()) {
-            case "chrome": driver = new ChromeDriver(); break;
-            case "firefox": driver =  new FirefoxDriver(); break;
-            case "edge" : driver =  new EdgeDriver(); break;
-            default: System.out.println("Invalid browser name provided: " + browser + ". Defaulting to Chrome."); return;
+        //BELOW CODE WILL EXECUTE FOR SELENIUM GRID
+        //WE SET BROWSER and OS
+        if(p.getProperty("execution_env").equalsIgnoreCase("remote")) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+
+            if(os.equalsIgnoreCase("windows"))
+                capabilities.setPlatform(Platform.WIN11);
+            else if(os.equalsIgnoreCase("mac"))
+                capabilities.setPlatform(Platform.MAC);
+            else {
+                System.out.println("Pls provide correct OS");
+                return;
+            }
+
+            switch (browser.toLowerCase()) {
+                case "chrome": capabilities.setBrowserName("chrome"); break;
+                case "firefox": capabilities.setBrowserName("firefox"); break;
+                case "edge" : capabilities.setBrowserName("MicrosoftEdge"); break;
+                default: System.out.println("Invalid browser name provided: " + browser ); return;
+            }
+            driver =  new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
+        }
+        //BELOW CODE WILL EXECUTE FOR LOCAL MACHINE
+        if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
+            switch (browser.toLowerCase()) {
+                case "chrome": driver = new ChromeDriver(); break;
+                case "firefox": driver =  new FirefoxDriver(); break;
+                case "edge" : EdgeOptions options = new EdgeOptions();
+                    options.addArguments("--remote-allow-origins=*"); // if needed
+                    driver = new EdgeDriver(options);
+                    break;
+                default: System.out.println("Invalid browser name provided: " + browser + ". Defaulting to Chrome."); return;
+            }
         }
 
         logger.debug("Deleting all cookies.");
